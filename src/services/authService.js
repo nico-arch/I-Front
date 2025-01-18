7408384a5175a892985c034842937b5e3382811a
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2025-01-18 18:06:13 +0000

    Some changes

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,12 +4,14 @@ const API_ENDPOINT = `${API_URL_V1}/auth`;
 
 export const login = async (email, password) => {
   try {
-    console.log("Api endpoint: "+ `${API_ENDPOINT}`);
+    console.log("Api endpoint: " + `${API_ENDPOINT}`);
     const response = await axios.post(`${API_ENDPOINT}/login`, {
       email,
       password,
     });
     const { token } = response.data;
+    //console.log("token :" + token);
+    console.log("login responses: " + { response });
 
     // Stocker le token dans le localStorage
     localStorage.setItem("token", token);
